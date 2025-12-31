f27ce539ffbc748e4931a268d4ee18bd71b189a6
owais1724 <168098735+owais1724@users.noreply.github.com>
2025-12-31 08:30:35 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,20 +1,12 @@
 from pydantic import BaseModel
-from typing import Optional
-
-# -------- User --------
-class User(BaseModel):
-    username: str
-    role: str
 
 
-# -------- Task Input --------
 class Task(BaseModel):
     title: str
     description: str
     completed: bool = False
 
 
-# -------- Task Output --------
 class TaskInDB(Task):
     id: str
     owner: str
