d06a4f8deb26ba70bdd0f35fe3950dfbd3cb58c1
owais1724 <168098735+owais1724@users.noreply.github.com>
2025-12-31 08:28:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,10 @@ from fastapi.security import OAuth2PasswordRequestForm
 from auth import login_user
 from tasks import router as task_router
 
-app = FastAPI(title="Task Management API with JWT")
+app = FastAPI(title="Task Management System with JWT")
 
-# -------- Login --------
 @app.post("/login")
 def login(form_data: OAuth2PasswordRequestForm = Depends()):
     return login_user(form_data)
 
-# -------- Include Routes --------
 app.include_router(task_router)
